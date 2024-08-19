93990d3881cb800aaa9b9997ca1a2bc73e233c9e
Dharmendrasingh9760 <152598648+Dharmendrasingh9760@users.noreply.github.com>
2024-08-19 22:41:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('hello word'); //write a response to the client
+  res.write('hello word Dharmendra singh'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
